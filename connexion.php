335ea0da9aaaed8db85bf595374988c64d942f8c
rhuchon <reynald.huchon@ensc.fr>
2014-03-05 15:38:52 +0100

    bob

--- connexion.php
+++ connexion.php
@@ -8,9 +8,19 @@ $LOGIN="root";
 $MDP="root";
 $MABASE="projet_web";
 
-$CONNEXION="mysql_connect($SERVEUR,$LOGIN,$MDP,$MABASE)";
+$CONNEXION=mysql_connect($SERVEUR,$LOGIN,$MDP);
 
 mysql_select_db($MABASE);
+
+$sql="SELECT * from personne";
+
+$resultats = mysql_query($sql);
+
+while ($ligne = mysql_fetch_assoc($resultats))
+{
+	$nom = $ligne['nom'];
+	echo "votre nom : ".$nom."";
+}
 
 
 ?>
--- recup2.php
+++ recup2.php
@@ -1,15 +1,16 @@
 <?php
-$CONNEXION="mysql_connect("localhost","root","root","projet_web")";
+require_once "connexion.php";
+
 // Check connection
-if (mysql_connect_errno())
+/*if (mysql_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysql_connect_error();
-  }
+  }*/
 
-$sql="INSERT INTO 'personne' ('nom', 'prenom', 'mdp', 'mail_pers', 'date_naissance', 'sexe')
+$sql="INSERT INTO personne (nom, prenom, mdp, mail_pers, date_naissance, sexe)
 VALUES
-('$_POST[nom]','$_POST[prenom]','$_POST[mdp]',$_POST[mail],$_POST[date_naissance],$_POST[sexe])";
-
+('".$_POST['nom']."','".$_POST['prenom']."','".$_POST['mdp']."','".$_POST['mail']."','".$_POST['date_naissance']."','".$_POST['sexe']."')";
+echo $sql;
 if (!mysql_query($CONNEXION,$sql))
   {
   die('Error: ' . mysql_error($CONNEXION));
