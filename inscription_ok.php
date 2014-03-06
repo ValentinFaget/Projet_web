0e0834177ec3ceebd955d029e563a2e89d4d848d
Valentin Faget <valentin.faget@ensc.fr>
2014-03-06 10:00:56 +0100

    ça avance grave!!

---
+++ inscription_ok.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+	<meta content="text/html; charset=UTF-8" http-equiv="content-type" />
+	<link href="form.css" rel="stylesheet" type="text/css" />
+	<link rel="icon" href="image/images.jpeg" type="image/x-icon"/>
+
+	<title>Erreur lors de l'inscription</title>
+</head>
+
+<body>
+
+	<header>
+		<img class="logo" src="image/sentez-vous_sport_2013.gif" alt="logo_site" width="100" heigth="100"/>
+		<div class="titre"> NIGGI'SPORT </div>
+</header>
+
+<br/>
+<h2 align="center">
+Inscription réussie! Bienvenue parmis nous! <br/>
+<a href="page_acceuil.html">Retour à la page d'acceuil</a>
+</h2>
+
+</body>
+</html>
--- recup2.php
+++ recup2.php
@@ -6,19 +6,26 @@ require_once "connexion.php";
   {
   echo "Failed to connect to MySQL: " . mysql_connect_error();
   }*/
+if ($nom=="" || $prenom=="" || $mdp=="" || $mail_pers=="" || $date_naissance=="")
+{
 
-$sql="INSERT INTO personne (nom, prenom, mdp, mail_pers, date_naissance, sexe)
-VALUES
-('".$_POST['nom']."','".$_POST['prenom']."','".$_POST['mdp']."','".$_POST['mail']."','".$_POST['date_naissance']."','".$_POST['sexe']."')";
-<<<<<<< HEAD
-mysql_query($sql);
+	header('Location: inscription_erreur.php');
 
-=======
-echo $sql;
-mysql_query($sql);
->>>>>>> c8849360fb826c3bf4ede3eda206f113969e01b0
+}
+else
+{
+	$sql="INSERT INTO personne (nom, prenom, mdp, mail_pers, date_naissance, sexe, role)
+	VALUES
+	('".$_POST['nom']."','".$_POST['prenom']."','".$_POST['mdp']."','".$_POST['mail']."','".$_POST['date_naissance']."','".$_POST['sexe']."','".$_POST['role']."')";
+	header('Location: inscription_ok.php');
+	/*<<<<<<< HEAD
+	mysql_query($sql);
 
-mysql_close($CONNEXION);
-
+	=======
+	echo $sql;
+	mysql_query($sql);
+	>>>>>>> c8849360fb826c3bf4ede3eda206f113969e01b0
+	mysql_close($CONNEXION);*/
+}
 
 ?>
