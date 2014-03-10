14392ab378c322e8d2ffbba29e7cc080a9eee1df
rhuchon <reynald.huchon@ensc.fr>
2014-03-10 12:53:31 +0100

    a

--- connecterpersonne.php
+++ connecterpersonne.php
@@ -3,6 +3,8 @@
 
 require "connexion.php";
 
+if($_POST['nom']==NULL || $_POST['prenom']==NULL || $_POST['mdp']==NULL)
+{
 if($_POST['nom']==NULL)
 	echo "rentrer votre nom <br/>";
 
@@ -13,16 +15,24 @@ if($_POST['prenom']==NULL)
 
 if($_POST['mdp']==NULL)
 	echo "rentrer votre mot de passe <br/>";
+}
 
 
-$sql="SELECT mdp FROM personne WHERE nom=$_POST['nom'] && prenom=$_POST['prenom']";
+else {
+$sql="SELECT mdp FROM personne WHERE nom='".$_POST['nom']."' && prenom='".$_POST['prenom']."' ";
 
 $sqlmdp=mysql_query($sql);
+$res = mysql_fetch_assoc($sqlmdp);
+$mdp = $res['mdp'];
 
-if ($_POST['mdp']=$sqlmdp)
-	echo "connexion réussie";
+if ($_POST['mdp']==$mdp)
+	{echo "connexion réussie";
+	echo "<meta http-equiv='Refresh' content='2; URL= page_accueil_participant.php' />";
+	}
 else echo " mauvais mot de passe";
 
+
+}
 
 
 
