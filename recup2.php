5a13e6150063c67fc1ff69d1edb8fe34e2bda87a
rhuchon <reynald.huchon@ensc.fr>
2014-03-10 08:54:19 +0100

    ok

--- recup2.php
+++ recup2.php
@@ -9,7 +9,7 @@ require_once "connexion.php";
 if ($nom=="" || $prenom=="" || $mdp=="" || $mail_pers=="" || $date_naissance=="")
 {
 
-<<<<<<< HEAD
+
 $sql="INSERT INTO personne (nom, prenom, mdp, mail_pers, date_naissance, sexe)
 VALUES
 ('".$_POST['nom']."','".$_POST['prenom']."','".$_POST['mdp']."','".$_POST['mail']."','".$_POST['date_naissance']."','".$_POST['sexe']."')";
@@ -32,15 +32,14 @@ else
 	VALUES
 	('".$_POST['nom']."','".$_POST['prenom']."','".$_POST['mdp']."','".$_POST['mail']."','".$_POST['date_naissance']."','".$_POST['sexe']."','".$_POST['role']."')";
 	header('Location: inscription_ok.php');
-	/*<<<<<<< HEAD
-	mysql_query($sql);
->>>>>>> 0e0834177ec3ceebd955d029e563a2e89d4d848d
+	
+	
 
-	=======
-	echo $sql;
+
+	
 	mysql_query($sql);
-	>>>>>>> c8849360fb826c3bf4ede3eda206f113969e01b0
-	mysql_close($CONNEXION);*/
+	
+	mysql_close($CONNEXION);
 }
 
 ?>
