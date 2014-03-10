483759c181810fbfe4110546a6583fe4f71f4b75
rhuchon <reynald.huchon@ensc.fr>
2014-03-10 12:47:10 +0100

    au calme

--- connexion.php
+++ connexion.php
@@ -12,7 +12,7 @@ $CONNEXION=mysql_connect($SERVEUR,$LOGIN,$MDP);
 
 mysql_select_db($MABASE);
 
-$sql="SELECT * from personne";
+/*$sql="SELECT * from personne";
 
 $resultats = mysql_query($sql);
 
@@ -20,7 +20,7 @@ while ($ligne = mysql_fetch_assoc($resultats))
 {
 	$nom = $ligne['nom'];
 	echo "votre nom : ".$nom."";
-}
+} */
 
 
 ?>
--- recup2.php
+++ recup2.php
@@ -7,50 +7,31 @@ require_once "connexion.php";
   echo "Failed to connect to MySQL: " . mysql_connect_error();
   }*/
 
+
 if ($_POST['nom']=="" || $_POST['prenom']=="" || $_POST['mdp']=="" || $_POST['mail']=="" )
 {
 
 
-$sql="INSERT INTO personne (nom, prenom, mdp, mail_pers, date_naissance, sexe)
-VALUES
-('".$_POST['nom']."','".$_POST['prenom']."','".$_POST['mdp']."','".$_POST['mail']."','".$_POST['date_naissance']."','".$_POST['sexe']."')";
 
-mysql_query($sql);
-
-
-echo $sql;
-mysql_query($sql);
-
-
-mysql_close($CONNEXION);
-=======
-	header('Location: inscription_erreur.php');
+	//header("Location: inscription_erreur.php");
+ echo "<meta http-equiv='Refresh' content='0; URL= inscription_erreur.php' />";
 
 }
 else
 {
-	$sql="INSERT INTO personne (nom, prenom, mdp, mail_pers, date_naissance, sexe, role)
+	$sql="INSERT INTO personne (nom, prenom, mdp, mail_pers, date_naissance, sexe, role, id_asso)
 	VALUES
-	('".$_POST['nom']."','".$_POST['prenom']."','".$_POST['mdp']."','".$_POST['mail']."','".$_POST['date_naissance']."','".$_POST['sexe']."','".$_POST['role']."')";
-<<<<<<< HEAD
-	header('Location: inscription_ok.php');
-	
-	
+	('".$_POST['nom']."','".$_POST['prenom']."','".$_POST['mdp']."','".$_POST['mail']."','".$_POST['date_naissance']."','".$_POST['sexe']."','".$_POST['role']."','1')";
 
-
-	
 	mysql_query($sql);
 	
-	mysql_close($CONNEXION);
-=======
 	
-	mysql_query($sql);
->>>>>>> 0e0834177ec3ceebd955d029e563a2e89d4d848d
-
+	//header('Location: inscription_ok.php');
+	echo "<meta http-equiv='Refresh' content='0; URL= inscription_ok.php' />";
 
 	mysql_close($CONNEXION);
-	header('Location: inscription_ok.php');
->>>>>>> 30d448e5ad9d1db55359a79fb4813b9f6174f0ec
+
+
 }
 
 ?>
